0e45bfe429f50a36e783bb4a58b25dd767cced16
sergnik1995 <37689397+sergnik1995@users.noreply.github.com>
2018-04-12 19:26:15 +0300

    update
    1) Улучшена читаемость кода.
    2) Переделаны функции вывода, вычисления средних значений.
    3) Добавлены новые классы по профессиям.

--- OOO_Vector.php
+++ OOO_Vector.php
@@ -4,313 +4,314 @@ error_reporting(-1);
 
 class Company
 {
-    private $departments;
+    public $departments;
+
     public function __construct(array $a)
     {
         $this->departments = $a;
     }
-    public function getOutput()
+
+    public function getAverageWorkers() 
     {
-        padRight('Департамент', 15);
-        padLeft('сотр.', 10);
-        padLeft('тугр.', 10);
-        padLeft('кофе', 10);
-        padLeft('стр.', 10);
-        padLeft('тугр./стр.', 10);
-        echo "\n".str_repeat("-", 70)."\n";
-        foreach($this->departments as $department){
-            $department->getOutput();
-            echo "\n";
-        }
-        padRight('Среднее', 15);
-        padLeft($this->getAverageWorkers(), 10);
-        padLeft($this->getAveragePay(), 10);
-        padLeft($this->getAverageCoffee(), 10);
-        padLeft($this->getAverageLists(), 10);
-        padLeft($this->getAveragePayInLists(), 10);
-        echo "\n";
-        padRight('Всего', 15);
-        padLeft($this->getTotalWorkers(), 10);
-        padLeft($this->getTotalPay(), 10);
-        padLeft($this->getTotalCoffee(), 10);
-        padLeft($this->getTotalLists(), 10);
-        padLeft($this->getTotalPayInLists(), 10);
-        echo "\n";
+        return $this->getTotalWorkers() / count($this->departments);
     }
-    public function getAverageWorkers(){
-        $count = 0;
-        $sum = 0;
-        foreach($this->departments as $department){
-            $sum += $department->sumWorkers();
-            $count++;
-        }
-        return round($sum / $count, 1);
+
+    public function getAveragePay()
+    {
+        return $this->getTotalPay() / count($this->departments);
     }
-    public function getAveragePay(){
-        $count = 0;
-        $sum = 0;
-        foreach($this->departments as $department){
-            $sum += $department->sumPay();
-            $count++;
-        }
-        return round($sum / $count, 1);
+
+    public function getAverageCoffee()
+    {
+        return $this->getTotalCoffee() / count($this->departments);
     }
-    public function getAverageCoffee(){
-        $count = 0;
-        $sum = 0;
-        foreach($this->departments as $department){
-            $sum += $department->sumCoffee();
-            $count++;
-        }
-        return round($sum / $count, 1);
+
+    public function getAverageLists()
+    {
+        return $this->getTotalLists() / count($this->departments);
     }
-    public function getAverageLists(){
-        $count = 0;
-        $sum = 0;
-        foreach($this->departments as $department){
-            $sum += $department->sumLists();
-            $count++;
-        }
-        return round($sum / $count, 1);
+
+    public function getAveragePayInLists()
+    {
+        return $this->getTotalPayInLists() / count($this->departments);
     }
-    public function getAveragePayInLists(){
-        $count = 0;
-        $sum = 0;
-        foreach($this->departments as $department){
-            $sum += $department->getPayInLists();
-            $count++;
-        }
-        return round($sum / $count, 1);
-    }
-    public function getTotalWorkers(){
+
+    public function getTotalWorkers()
+    {
         $sum = 0;
         foreach($this->departments as $department){
             $sum += $department->sumWorkers();
         }
         return $sum;
     }
-    public function getTotalPay(){
+
+    public function getTotalPay()
+    {
         $sum = 0;
         foreach($this->departments as $department){
             $sum += $department->sumPay();
         }
         return $sum;
     }
-    public function getTotalCoffee(){
+
+    public function getTotalCoffee()
+    {
         $sum = 0;
         foreach($this->departments as $department){
             $sum += $department->sumCoffee();
         }
         return $sum;
     }
-    public function getTotalLists(){
+
+    public function getTotalLists()
+    {
         $sum = 0;
         foreach($this->departments as $department){
             $sum += $department->sumLists();
         }
         return $sum;
     }
-    public function getTotalPayInLists(){
+
+    public function getTotalPayInLists()
+    {
         $sum = 0;
         foreach($this->departments as $department){
             $sum += $department->getPayInLists();
         }
         return $sum;
     }
+
 }
 
 class Department
 {
-    private $name;
-    private $workers;
-    public function __construct(string $a, array $b){
+    public $name;
+    public $workers;
+
+    public function __construct(string $a, array $b)
+    {
         $this->name = $a;
         $this->workers = $b;
     }
-    public function sumWorkers(){
-        $count = 0;
-        foreach($this->workers as $worker){
-            $count++;
-        }
-        return $count;
+
+    public function sumWorkers()
+    {
+        return count($this->workers);
     }
-    public function sumPay(){
+
+    public function sumPay()
+    {
         $sumPay = 0;
-        foreach($this->workers as $worker){
+        foreach($this->workers as $worker) {
             $sumPay += $worker->getPay();
         }
         return $sumPay;
     }
-    public function sumCoffee(){
+
+    public function sumCoffee()
+    {
         $sumCoffee = 0;
-        foreach($this->workers as $worker){
+        foreach($this->workers as $worker) {
             $sumCoffee += $worker->getCoffee();
         }
         return $sumCoffee;
     }
-    public function sumLists(){
+
+    public function sumLists()
+    {
         $sumLists = 0;
-        foreach($this->workers as $worker){
+        foreach($this->workers as $worker) {
             $sumLists += $worker->getLists();
         }
         return $sumLists;
     }
-    public function getPayInLists(){
+
+    public function getPayInLists()
+    {
         $sumPay = $this->sumPay();
         $sumLists = $this->sumLists();
-        return round($sumPay / $sumLists, 1);
+        return $sumPay / $sumLists;
     }
-    public function getOutput(){
-        padRight($this->name, 15);
-        padLeft($this->sumWorkers(), 10);
-        padLeft($this->sumPay(), 10);
-        padLeft($this->sumCoffee(), 10);
-        padLeft($this->sumLists(), 10);
-        padLeft($this->getPayInLists(), 10);
-    }
+
 }
 
 class Worker
 {
-    private $prof;
     private $rang;
     private $boss;
-    public function __construct(string $a, int $b, bool $c){
-        $this->prof = $a;
-        $this->rang = $b;
-        $this->boss = $c;
+
+    public function __construct(int $rang, int $boss)
+    {
+    	$this->rang = $rang;
+    	$this->boss = $boss;
     }
-    public function getPay(){
-        $pay;
-        if($this->prof == "me") {
-            $pay = 500;
-        }
-        else if($this->prof == "ma") {
-            $pay = 400;
-        }
-        else if($this->prof == "in") {
-            $pay = 200;
-        }
-        else if($this->prof == "an") {
-            $pay = 800;
-        }
+
+    public function getPay(): int
+    {
         if($this->rang == 2) {
-            $pay *= 1.25;
-        }
-        else if($this->rang == 3) {
-            $pay *= 1.5;
-        }
-        if($this->boss == 1) {
-            $pay *= 1.5;
-        }
-        return $pay;
-    }
-    public function getCoffee(){
-        $coffee;
-        if($this->prof == "me") {
-            $coffee = 20;
-        }
-        else if($this->prof == "ma") {
-            $coffee = 15;
-        }
-        else if($this->prof == "in") {
-            $coffee = 5;
-        }
-        else if($this->prof == "an") {
-            $coffee = 50;
+            $this->pay *= 1.25;
+        } else if($this->rang == 3) {
+            $this->pay *= 1.5;
         }
         if($this->boss == 1) {
-            $coffee *= 2;
+            $this->pay *= 1.5;
         }
-        return $coffee;
+        return $this->pay;
     }
-    public function getLists(){
-        $lists;
+
+    public function getCoffee(): int
+    {
+        if($this->boss == 1) {
+            $this->coffee *= 2;
+        }
+        return $this->coffee;
+    }
+
+    public function getLists(): int
+    {
         if($this->boss == 1){
             return 0;
         }
-        else if($this->prof == "me") {
-            $lists = 200;
-        }
-        else if($this->prof == "ma") {
-            $lists = 150;
-        }
-        else if($this->prof == "in") {
-            $lists = 50;
-        }
-        else if($this->prof == "an") {
-            $lists = 5;
-        }
-        return $lists;
+        return $this->lists;
     }
 }
 
-function padRight($string, $length){
-    $strLen = mb_strlen($string);
-    echo $string;
-    echo str_repeat(" ", $length - $strLen);
+class Manager extends Worker
+{
+	public $pay = 500;
+	public $coffee = 20;
+	public $lists = 200;
 }
 
-function padLeft($string, $length){
-    $strLen = mb_strlen($string);
-    echo str_repeat(" ", $length - $strLen);
-    echo $string;
+class Marketer extends Worker
+{
+	public $pay = 400;
+	public $coffee = 15;
+	public $lists = 150;
 }
 
-function createDepartment(){
+class Engineer extends Worker 
+{
+	public $pay = 200;
+	public $coffee = 5;
+	public $lists = 50;
+}
+
+class Analyst extends Worker 
+{
+	public $pay = 800;
+	public $coffee = 50;
+	public $lists = 5;
+}
+
+function padRight($string, $length): string
+{
+    $strLen = mb_strlen($string);
+    $string = $string . str_repeat(" ", $length - $strLen);
+    return $string;
+}
+
+function padLeft($string, $length): string 
+{
+    $strLen = mb_strlen($string);
+    $string = str_repeat(" ", $length - $strLen) . $string;
+    return $string;
+}
+
+function getCompanyOutput(Company $company)
+{
+	echo padRight('Департамент', 15);
+    echo padLeft('сотр.', 10);
+	echo padLeft('тугр.', 10);
+    echo padLeft('кофе', 10);
+    echo padLeft('стр.', 10);
+    echo padLeft('тугр./стр.', 10);
+    echo "\n".str_repeat("-", 70)."\n";
+    foreach($company->departments as $department){
+    	getDepartmentOutput($department);
+        echo "\n";
+    }
+    echo padRight('Среднее', 15);
+    echo padLeft(round($company->getAverageWorkers(), 1), 10);
+    echo padLeft(round($company->getAveragePay(), 1), 10);
+    echo padLeft(round($company->getAverageCoffee(), 1), 10);
+    echo padLeft(round($company->getAverageLists(), 1), 10);
+    echo padLeft(round($company->getAveragePayInLists(), 1), 10);
+    echo "\n";
+    echo padRight('Всего', 15);
+    echo padLeft($company->getTotalWorkers(), 10);
+    echo padLeft($company->getTotalPay(), 10);
+    echo padLeft($company->getTotalCoffee(), 10);
+    echo padLeft($company->getTotalLists(), 10);
+    echo padLeft(round($company->getTotalPayInLists(), 1), 10);
+    echo "\n";
+}
+
+function getDepartmentOutput(Department $department)
+{
+	echo padRight($department->name, 15);
+	echo padLeft($department->sumWorkers(), 10);
+	echo padLeft($department->sumPay(), 10);
+	echo padLeft($department->sumCoffee(), 10);
+	echo padLeft($department->sumLists(), 10);
+	echo padLeft(round($department->getPayInLists(), 1), 10);
+}
+
+function createDepartment()
+{
     $workers1 = array();
     for($i = 0; $i < 9; $i++){
-        $workers1[$i] = new Worker("me", 1, 0);
+        $workers1[$i] = new Manager(1, 0);
     }
     for($i = 9; $i < 12; $i++) {
-        $workers1[$i] = new Worker("me", 2, 0);
+        $workers1[$i] = new Manager(2, 0);
     }
     for($i = 12; $i < 14; $i++) {
-        $workers1[$i] = new Worker("me", 3, 0);
+        $workers1[$i] = new Manager(3, 0);
     }
     for($i = 14; $i < 16; $i++) {
-        $workers1[$i] = new Worker("ma", 1, 0);
+        $workers1[$i] = new Marketer(1, 0);
     }
-    $workers1[16] = new Worker("me", 2, 1);
+    $workers1[16] = new Manager(2, 1);
     $workers2 = array();
     for($i = 0; $i < 12; $i++) {
-        $workers2[$i] = new Worker("me", 1, 0);
+        $workers2[$i] = new Manager(1, 0);
     }
     for($i = 12; $i < 18; $i++) {
-        $workers2[$i] = new Worker("ma", 1, 0);
+        $workers2[$i] = new Marketer(1, 0);
     }
     for($i = 18; $i < 21; $i++) {
-        $workers2[$i] = new Worker("an", 1, 0);
+        $workers2[$i] = new Analyst(1, 0);
     }
     for($i = 21; $i < 23; $i++) {
-        $workers2[$i] = new Worker("an", 2, 0);
+        $workers2[$i] = new Analyst(2, 0);
     }
-    $workers2[23] = new Worker('ma', 2, 1);
+    $workers2[23] = new Marketer(2, 1);
     $workers3 = array();
     for($i = 0; $i < 15; $i++) {
-        $workers3[$i] = new Worker("ma", 1, 0);
+        $workers3[$i] = new Marketer(1, 0);
     }
     for($i = 15; $i < 25; $i++) {
-        $workers3[$i] = new Worker("ma", 2, 0);
+        $workers3[$i] = new Marketer(2, 0);
     }
     for($i = 25; $i < 33; $i++) {
-        $workers3[$i] = new Worker("me", 1, 0);
+        $workers3[$i] = new Manager(1, 0);
     }
     for($i = 33; $i < 35; $i++) {
-        $workers3[$i] = new Worker("in", 1, 0);
+        $workers3[$i] = new Engineer(1, 0);
     }
-    $workers3[$i] = new Worker('ma', 3, 1);
+    $workers3[$i] = new Marketer(3, 1);
     $workers4 = array();
     for($i = 0; $i < 13; $i++) {
-        $workers4[$i] = new Worker("me", 1, 0);
+        $workers4[$i] = new Manager(1, 0);
     }
     for($i = 13; $i < 18; $i++) {
-        $workers4[$i] = new Worker("me", 2, 0);
+        $workers4[$i] = new Manager(2, 0);
     }
     for($i = 18; $i < 23; $i++) {
-        $workers4[$i] = new Worker("in", 1, 0);
+        $workers4[$i] = new Engineer( 1, 0);
     }
-    $workers4[23] = new Worker('me', 1, 1);
+    $workers4[23] = new Manager(1, 1);
     $departments = array();
     $departments[1] = new Department("Закупок", $workers1);
     $departments[2] = new Department("Продаж", $workers2);
@@ -321,4 +322,4 @@ function createDepartment(){
 }
 
 $company = createDepartment();
-$company->getOutput();
+getCompanyOutput($company);
