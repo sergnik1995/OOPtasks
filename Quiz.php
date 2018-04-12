fde21efc8c5ce3feec5ab97b4ebb5d1b2eac0543
sergnik1995 <37689397+sergnik1995@users.noreply.github.com>
2018-04-12 13:53:21 +0300

    update
    Улучшена читаемость кода.

--- Quiz.php
+++ Quiz.php
@@ -6,52 +6,71 @@ mb_internal_encoding('utf-8');
 abstract class AbstractQuestion 
 {
     public $text;
-    public $correctOption; // ответ
+    public $correctOption;
+
     abstract function checkAnswer($answer);
     abstract function getQuestionOutput();
 }
 
 class ChoiceQuestion extends AbstractQuestion
 {
-    public $options = array(); // варианты ответа
-    public function __construct($stringText, $stringCorrect, $arrayOptions){
-        $this->text = $stringText;
-        $this->correctOption = $stringCorrect;
-        $this->options = $arrayOptions;
-    }
-    public function checkAnswer($answer){
-        if($this->correctOption == $answer) return 1;
-        else return 0;
+    public $options = array();
 
+    public function __construct(string $text, string $correctOption, array $options)
+    {
+        $this->text = $text;
+        $this->correctOption = $correctOption;
+        $this->options = $options;
     }
-    public function getQuestionOutput(){
+
+    public function checkAnswer($answer)
+    {
+        if($this->correctOption == $answer) {
+        	return 1;
+        }
+        return 0;
+    }
+
+    public function getQuestionOutput()
+    {
         echo $this->text . "\n";
-        foreach($this->options as $key => $option){
+        foreach($this->options as $key => $option) {
             echo $key . ") " . $option . " ";
         }
         echo "\n";
     }
+
 }
 
 class NumericQuestion extends AbstractQuestion
 {
-    public $deviation; // допустимая погрешность
-    public function __construct($stringText, $stringCorrect, $numberDeviation){
-        $this->text = $stringText;
-        $this->correctOption = $stringCorrect;
-        $this->deviation = $numberDeviation;
+    public $deviation;
+
+    public function __construct(string $text, float $correctOption, int $deviation)
+    {
+        $this->text = $text;
+        $this->correctOption = $correctOption;
+        $this->deviation = $deviation;
     }
-    public function checkAnswer($answer){
+
+    public function checkAnswer($answer)
+    {
         $answer = round($answer, $this->deviation);
-        if($this->correctOption == $answer) return 1;
-        else return 0;
+        if($this->correctOption == $answer) {
+        	return 1;
+        }
+        return 0;
     }
-    public function getQuestionOutput(){
+
+    public function getQuestionOutput()
+    {
         echo $this->text . "\n";
     }
+
 }
 
-function createQuestions(){
+function createQuestions()
+{
     $temp = array("a" => "Альберт Эйнштейн", "b" => "Нильс Бор", "c" => "Макс Планк", "d" => "Лайнус Полинг");
     $q1 = new ChoiceQuestion("Кто из этих ученых дважды был лауреатом Нобелевской премии?",
                              "d", $temp);
@@ -63,12 +82,10 @@ function createQuestions(){
 $questions = createQuestions();
 $answers = array('d', 3.14);
 foreach($questions as $key => $question) {
-    //var_dump($answers);
     $question->getQuestionOutput();
-    if($question->checkAnswer($answers[$key])){
+    if($question->checkAnswer($answers[$key])) {
         echo "Вы выбрали правильный ответ!\n";
-    }
-    else{
+    } else {
         echo "Вы выбрали неправильный ответ. Правильный ответ - {$question->correctOption}.\n";
     }
 }
